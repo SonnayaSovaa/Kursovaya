9b0adb5b37f90dd1482a3310a23d69cd19554e3a
Ксения Нагишева <knagisheva@gmail.com>
2024-03-10 21:45:28 +0300

    Выбор сложности через PlayerPrefs

--- Assets/Scripts/DropDown.cs
+++ Assets/Scripts/DropDown.cs
@@ -13,23 +13,32 @@ public class DropDown : MonoBehaviour
     private void Start()
     {
         opisanie.text = opisanie.text = "—лабые и медленные враги, много лечени€, меньша€ цель.";
+        SetInt("LevelDif", 0);
     }
     public void ValChange()
     {
-        switch(drdw.value)
+        switch (drdw.value)
         {
             case 0:
                 opisanie.text = "—лабые и медленные враги, много лечени€, меньша€ цель.";
+                SetInt("LevelDif", 0);
                 break;
 
             case 1:
                 opisanie.text = " репкие и уверенные враги, достаточное количество лечени€, нормальна€ цель.";
+                SetInt("LevelDif", 1);
                 break;
 
             case 2:
                 opisanie.text = "—ильные и быстрые враги, мало лечени€, больша€ цель.";
+                SetInt("LevelDif", 2);
                 break;
 
         }
+    }
+
+    public void SetInt(string KeyName, int Value)
+    {
+        PlayerPrefs.SetInt(KeyName, Value);
     }
 }
---
+++ Assets/Scripts/LevDif.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevDif : MonoBehaviour
+{
+    public GameObject Lev1;
+    public GameObject Lev2;
+    public GameObject Lev3;
+
+    private void Awake()
+    {
+        int lev = GetLev("LevelDif");
+
+        switch (lev)
+        {
+            case 0:
+                Lev1.SetActive(true);
+                break;
+            case 1:
+                Lev2.SetActive(true);
+                break;
+            case 2:
+                Lev3.SetActive(true);
+                break;
+        }
+    }
+
+    public int GetLev(string KeyName)
+    {
+        return PlayerPrefs.GetInt(KeyName);
+    }
+    
+}
