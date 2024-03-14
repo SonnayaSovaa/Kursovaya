abe4a32fdc3e75fa9aefb35e6ad904675d1bf44c
DealtMin <dealtmin@gmail.com>
2024-03-14 15:22:54 +0300

    ИИ следует за передвигающийся целью

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 using System.Collections;
 public class MoveTo : MonoBehaviour
 {
     // Положение точки назначения
     public Transform goal;
+    private UnityEngine.AI.NavMeshAgent agent;
     void Start()
     {
         // Получение компонента агента
-        UnityEngine.AI.NavMeshAgent agent
+        agent
             = GetComponent<UnityEngine.AI.NavMeshAgent>();
         // Указаие точки назначения
+        agent.destination = goal.position;
+    }
+
+    private void Update()
+    {
         agent.destination = goal.position;
     }
 }
