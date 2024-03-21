73b9443812e644f4f35725f2a1434559b5756775
DealtMin <dealtmin@gmail.com>
2024-03-21 16:06:11 +0300

    руки в меню, возможно работают

---
+++ Assets/Scripts/PlayerImput.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerImput : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private InputActionProperty _gripAction;
+    [SerializeField] private InputActionProperty _activateAction;
+    void Update()
+    {
+        var gripvalue = _gripAction.action.ReadValue<float>();
+        var activationvalue = _gripAction.action.ReadValue<float>();
+        _animator.SetFloat("Grip",gripvalue);
+        _animator.SetFloat("Tigger",gripvalue);
+    }
+}
