8eb50c38c026c7f420968c4768a8afa742e37095
DealtMin <dealtmin@gmail.com>
2024-03-30 14:59:06 +0300

    работает!!!!!

--- Assets/Scripts/Rayyy.cs
+++ Assets/Scripts/Rayyy.cs
@@ -9,42 +9,13 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Rayyy : MonoBehaviour
 {
-    public bool TryGetHitInfo(out Vector3 position, out Vector3 normal, out int positionInLine, out bool isValidTarget)
-    {
-        position = default;
-        normal = default;
-        positionInLine = default;
-        isValidTarget = default;
+   private void Update()
+   {
+      //
+   }
 
-        if (! XRRayInteractor.TryGetCurrentRaycast(
-                out var raycastHit,
-                out var raycastHitIndex,
-                out var raycastResult,
-                out var raycastResultIndex,
-                out var isUIHitClosest))
-        {
-            return false;
-        }
-
-        if (raycastResult.HasValue && isUIHitClosest)
-        {
-            position = raycastResult.Value.worldPosition;
-            normal = raycastResult.Value.worldNormal;
-            positionInLine = raycastResultIndex;
-
-            isValidTarget = raycastResult.Value.gameObject != null;
-        }
-        else if (raycastHit.HasValue)
-        {
-            position = raycastHit.Value.point;
-            normal = raycastHit.Value.normal;
-            positionInLine = raycastHitIndex;
-
-            // Determine if the collider is registered as an interactable and the interactable is being hovered
-            isValidTarget = interactionManager.TryGetInteractableForCollider(raycastHit.Value.collider, out var interactable) &&
-                            IsHovering(interactable);
-        }
-
-        return true;
-    }
+   public void OnTriggerEnter(Collider other)
+   {
+      Debug.Log(other.tag);
+   }
 }
