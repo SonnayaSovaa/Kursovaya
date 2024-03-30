2ccb8c2acb22aa877f0731e3612bc67e5d588e3f
DealtMin <dealtmin@gmail.com>
2024-03-30 14:24:53 +0300

    какое-то АААААААА

--- Assets/Scripts/Rayyy.cs
+++ Assets/Scripts/Rayyy.cs
@@ -1,19 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.XR.OpenVR;
 using UnityEngine.XR.OpenXR.Features.Interactions;
 using Unity.XR.Oculus.Input;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Rayyy : MonoBehaviour
 {
-    [SerializeField] Transform L_orig;
-    [SerializeField] Transform R_orig;
-
-    [SerializeField] RaycastHit hit;
-    public OculusTouchController a;
-    private void Awake()
+    public bool TryGetHitInfo(out Vector3 position, out Vector3 normal, out int positionInLine, out bool isValidTarget)
     {
-        //
+        position = default;
+        normal = default;
+        positionInLine = default;
+        isValidTarget = default;
+
+        if (! XRRayInteractor.TryGetCurrentRaycast(
+                out var raycastHit,
+                out var raycastHitIndex,
+                out var raycastResult,
+                out var raycastResultIndex,
+                out var isUIHitClosest))
+        {
+            return false;
+        }
+
+        if (raycastResult.HasValue && isUIHitClosest)
+        {
+            position = raycastResult.Value.worldPosition;
+            normal = raycastResult.Value.worldNormal;
+            positionInLine = raycastResultIndex;
+
+            isValidTarget = raycastResult.Value.gameObject != null;
+        }
+        else if (raycastHit.HasValue)
+        {
+            position = raycastHit.Value.point;
+            normal = raycastHit.Value.normal;
+            positionInLine = raycastHitIndex;
+
+            // Determine if the collider is registered as an interactable and the interactable is being hovered
+            isValidTarget = interactionManager.TryGetInteractableForCollider(raycastHit.Value.collider, out var interactable) &&
+                            IsHovering(interactable);
+        }
+
+        return true;
     }
 }
