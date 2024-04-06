df8960a1bb6526f38ccd6676b205fd49ecec6af6
DealtMin <dealtmin@gmail.com>
2024-04-07 01:28:44 +0300

    навалила кринжа для анимации

--- Assets/Scripts/PlayerImput.cs
+++ Assets/Scripts/PlayerImput.cs
@@ -2,37 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerImput : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private InputActionProperty _gripAction;
     [SerializeField] private InputActionProperty _triggerAction;
+    
     void Update()
     {
-        var gripvalue = _gripAction.action.ReadValue<float>();
-        var activationvalue = _triggerAction.action.ReadValue<float>();
-        _animator.SetFloat("Grip",gripvalue);
-        _animator.SetFloat("Trigger", activationvalue);
-
-        /* if(TagDetecter.hoverTag==38)
-        {
-            _animator.SetBool("Hill", true);
-            //Debug.Log("heal");
-        }
-        else
-        {
-            _animator.SetBool("Hill", false);
-            if (TagDetecter.hoverTag == 39)
-            {
+            var gripvalue = _gripAction.action.ReadValue<float>();
+            var activationvalue = _triggerAction.action.ReadValue<float>();
+            _animator.SetFloat("Grip",gripvalue);
+            _animator.SetFloat("Trigger", activationvalue);
+        
+        
+           /* _animator.SetBool("Hill", false);
+            _animator.SetBool("Key", false);
+            _animator.SetFloat("Grip",gripvalue);
+            _animator.SetFloat("Trigger", activationvalue);
+            
+                _animator.SetBool("Hill", true);
+                //Debug.Log("heal");
+           
+                    _animator.SetBool("Key", true);
+                    //Debug.Log("key");
                 _animator.SetFloat("Grip", gripvalue);
-                //Debug.Log("key");
-            }
-            else
-            {
-                _animator.SetFloat("Grip", gripvalue);
-                //Debug.Log("weapon");
-            }
-        }*/
+                    //Debug.Log("weapon");*/
+                
     }
 }
