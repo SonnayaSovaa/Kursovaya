f94859a55105f97cf242656ee1a211663969476c
DealtMin <150230217+DealtMin@users.noreply.github.com>
2024-04-11 18:55:47 +0300

    magic kvadrat
    RABOTAYEEEEEEET

--- Assets/Scripts/MagicCube.cs
+++ Assets/Scripts/MagicCube.cs
@@ -1,44 +1,73 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Serialization;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MagicCube : MonoBehaviour
 {
 
     public GameObject[] points = new GameObject[9];
-    int[] vals = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int[] vals = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    bool che;
 
     private void Update()
     {
-        bool r = Check();
-        Debug.Log("MAGIC      " + r);
+        Check();
+        
     }
-    
 
-    private bool Check()
+
+    private void Check()
     {
-        int i = 0;
-        foreach (var point in points)
+        int was= 0;
+
+        foreach (GameObject point in points)
         {
-            Transform a = point.GetComponentInChildren<Transform>();
-            if (a==null)
-                vals[i] = 0;
-            else
-                vals[i]=Convert.ToInt32(a.name);
+            Transform[] childs = point.GetComponentsInChildren<Transform>();
+            foreach (Transform chil in childs)
+                if (childs.Length!=0 && chil.name.Contains("Attach")) //
+                {
+                    int  a = Convert.ToInt32(Convert.ToString(chil.name[1]));
+
+                   vals[a-1] =Convert.ToInt32(point.name);
+                    was++;
+                    break;
+                }
+
+            Debug.Log("WAS   " + point.GetComponentInParent<Transform>().name);
         }
 
 
-        bool hor = (vals[0]+vals[1]+vals[2]== vals[3] + vals[4] + vals[5] && vals[6] + vals[7] + vals[8] == vals[0] + vals[1] + vals[2]);
-        bool ver = (vals[0] + vals[3] + vals[6] == vals[1] + vals[4] + vals[7] && vals[0] + vals[3] + vals[6] == vals[2] + vals[5] + vals[8]);
-        bool dia = (vals[0] + vals[8]== vals[2] + vals[7]);
+        bool hor=false, ver=false, dia = false;
 
 
-        return hor&&ver&&dia&&vals[0]!=0 && vals[1] != 0 && vals[2] != 0 && vals[3] != 0 && vals[4] != 0 && vals[5] != 0 && vals[6] != 0 && vals[7] != 0 && vals[8] != 0;
+        if (was ==9)
+        {
+            hor = (vals[0] + vals[1] + vals[2] == vals[3] + vals[4] + vals[5] && vals[6] + vals[7] + vals[8] == vals[0] + vals[1] + vals[2]);
+            ver = (vals[0] + vals[3] + vals[6] == vals[1] + vals[4] + vals[7] && vals[0] + vals[3] + vals[6] == vals[2] + vals[5] + vals[8]);
+            dia = (vals[0] + vals[8] == vals[2] + vals[6]);
 
+            Debug.Log("HOR   " + hor);
+            Debug.Log("VER   " + ver);
+            Debug.Log("DIA   " + dia);
+
+            if (hor && ver && dia && vals[0] != 0 && vals[1] != 0 && vals[2] != 0 && vals[3] != 0 && vals[4] != 0 && vals[5] != 0 && vals[6] != 0 && vals[7] != 0 && vals[8] != 0)
+            {
+                Debug.Log("MAGIC   +++");
+            }
+            else Debug.Log("MAGIC  ---");
+        }
+
+
+
+       
 
     }
+    
 
 }
