cb8a6049e4ba9fe57c733dbe4bd5ddfe82728ea5
Ксения Нагишева <knagisheva@gmail.com>
2024-04-20 21:52:29 +0300

    таймер и небольшие корректировки ui

--- Assets/Scripts/Weap_Desc.cs
+++ Assets/Scripts/Weap_Desc.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.XR.CoreUtils;
@@ -14,7 +12,6 @@ public class Weap_Desc : MonoBehaviour
     [SerializeField] TMP_Text timer;
 
     float tim=0;
-
     int real_uron;
 
     public GameObject[] weapons;
@@ -24,10 +21,11 @@ public class Weap_Desc : MonoBehaviour
 
     public void Update()
     {
-        //timer += Time.deltaTime;
-        //timer.text = "ф"+Convert.ToString(Time.time);
+        tim += Time.deltaTime;
+        
+        timer.text = $"{Convert.ToInt32(tim/3600)}:{Convert.ToInt32(Math.Floor(tim%3600/60))}:{Convert.ToInt32(tim%60)}";
 
-        //Debug.Log(timer);
+        Debug.Log(timer);
     }
 
     public void InHand()
@@ -53,225 +51,225 @@ public class Weap_Desc : MonoBehaviour
                 switch (cur_tag)
                 {
                     case 0:
-                        description.text = "Описание: Довольно обычная крепкая булава.";
-                        rank.text = "Ранг: 2";
-                        uron.text = "Урон: 40";
+                        description.text = "Довольно обычная крепкая булава.";
+                        rank.text = "2";
+                        uron.text = "40";
                         real_uron = 40;
                         break;
                     case 1:
-                        description.text = "Описание: Простое аккуратное копьё.";
-                        rank.text = "Ранг: 2";
-                        uron.text = "Урон: 30";
+                        description.text = "Простое аккуратное копьё.";
+                        rank.text = "2";
+                        uron.text = "30";
                         real_uron = 30;
                         break;
                     case 2:
-                        description.text = "Описание: Копьё, пропитанное слабой тёмной магией.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 60";
+                        description.text = "Копьё, пропитанное слабой тёмной магией.";
+                        rank.text = "3";
+                        uron.text = "60";
                         real_uron = 60;
                         break;
                     case 3:
-                        description.text = "Описание: Магическо ядовитое копьё.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 80";
+                        description.text = "Магическо ядовитое копьё.";
+                        rank.text = "4";
+                        uron.text = "80";
                         real_uron = 6;
                         break;
                     case 4:
-                        description.text = "Описание: Зачарованный крабовый меч.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 75";
+                        description.text = "Зачарованный крабовый меч.";
+                        rank.text = "4";
+                        uron.text = "75";
                         real_uron = 75;
                         break;
                     case 5:
-                        description.text = "Описание: Неотёсанный меч космического мага.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 55";
+                        description.text = "Неотёсанный меч космического мага.";
+                        rank.text = "3";
+                        uron.text = "55";
                         real_uron = 55;
                         break;
                     case 6:
-                        description.text = "Описание: Загадочное копьё глубин.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 70";
+                        description.text = "Загадочное копьё глубин.";
+                        rank.text = "4";
+                        uron.text = "70";
                         real_uron = 70;
                         break;
                     case 7:
-                        description.text = "Описание: Неотёсанный меч светлого монаха.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 55";
+                        description.text = "Неотёсанный меч светлого монаха.";
+                        rank.text = "3";
+                        uron.text = "55";
                         real_uron = 55;
                         break;
                     case 8:
-                        description.text = "Описание: Костяная дубина лича.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 110";
+                        description.text = "Костяная дубина лича.";
+                        rank.text = "6";
+                        uron.text = "110";
                         real_uron = 110;
                         break;
                     case 9:
-                        description.text = "Описание: Меч гоблина-некроманта.";
-                        rank.text = "Ранг: 5";
-                        uron.text = "Урон: 85";
+                        description.text = "Меч гоблина-некроманта.";
+                        rank.text = "5";
+                        uron.text = "85";
                         real_uron = 85;
                         break;
                     case 10:
-                        description.text = "Описание: Меч гоблина Огненных Долин.";
-                        rank.text = "Ранг: 5";
-                        uron.text = "Урон: 85";
+                        description.text = "Меч гоблина Огненных Долин.";
+                        rank.text = "5";
+                        uron.text = "85";
                         real_uron = 85;
                         break;
                     case 11:
-                        description.text = "Описание: Рубило 'Монолит'.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 100";
+                        description.text = "Рубило 'Монолит'.";
+                        rank.text = "6";
+                        uron.text = "100";
                         real_uron = 100;
                         break;
                     case 12:
-                        description.text = "Описание: Поднятый с глубин меч Атлантиды.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 50";
+                        description.text = "Поднятый с глубин меч Атлантиды.";
+                        rank.text = "3";
+                        uron.text = "50";
                         real_uron = 50;
                         break;
                     case 13:
-                        description.text = "Описание: Разочарованный меч тьмы.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 95";
+                        description.text = "Разочарованный меч тьмы.";
+                        rank.text = "6";
+                        uron.text = "95";
                         real_uron = 95;
                         break;
                     case 14:
-                        description.text = "Описание: Меч драконьего дыхания.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 95";
+                        description.text = "Меч драконьего дыхания.";
+                        rank.text = "6";
+                        uron.text = "95";
                         real_uron = 95;
                         break;
                     case 15:
-                        description.text = "Описание: Паровая колотушка.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 95";
+                        description.text = "Паровая колотушка.";
+                        rank.text = "6";
+                        uron.text = "95";
                         real_uron = 95;
                         break;
                     case 16:
-                        description.text = "Описание: Меч ледяного краба.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 75";
+                        description.text = "Меч ледяного краба.";
+                        rank.text = "4";
+                        uron.text = "75";
                         real_uron = 75;
                         break;
                     case 17:
-                        description.text = "Описание: Резак спрутов-киборгов.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 55";
+                        description.text = "Резак спрутов-киборгов.";
+                        rank.text = "3";
+                        uron.text = "55";
                         real_uron = 55;
                         break;
                     case 18:
-                        description.text = "Описание: Лунный факел Олимпа.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 45";
+                        description.text = "Лунный факел Олимпа.";
+                        rank.text = "3";
+                        uron.text = "45";
                         real_uron = 45;
                         break;
                     case 19:
-                        description.text = "Описание: Прямой резак тёмного утра.";
-                        rank.text = "Ранг: 2";
-                        uron.text = "Урон: 30";
+                        description.text = "Прямой резак тёмного утра.";
+                        rank.text = "2";
+                        uron.text = "30";
                         real_uron = 30;
                         break;
                     case 20:
-                        description.text = "Описание: Прямой резак сетлого вечера.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 45";
+                        description.text = "Прямой резак сетлого вечера.";
+                        rank.text = "3";
+                        uron.text = "45";
                         real_uron = 45;
                         break;
                     case 21:
-                        description.text = "Описание: Древний меч Замёрзшего океана.";
-                        rank.text = "Ранг: 5";
-                        uron.text = "Урон: 75";
+                        description.text = "Древний меч Замёрзшего океана.";
+                        rank.text = "5";
+                        uron.text = "75";
                         real_uron = 75;
                         break;
                     case 22:
-                        description.text = "Описание: Неожиданно простой боевой топор.";
-                        rank.text = "Ранг: 2";
-                        uron.text = "Урон: 25";
+                        description.text = "Неожиданно простой боевой топор.";
+                        rank.text = "2";
+                        uron.text = "25";
                         real_uron = 25;
                         break;
                     case 23:
-                        description.text = "Описание: Топор Прожигающего Света.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 65";
+                        description.text = "Топор Прожигающего Света.";
+                        rank.text = "4";
+                        uron.text = "65";
                         real_uron = 65;
                         break;
                     case 24:
-                        description.text = "Описание: Качественный топор.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 50";
+                        description.text = "Качественный топор.";
+                        rank.text = "3";
+                        uron.text = "50";
                         real_uron = 6;
                         break;
                     case 25:
-                        description.text = "Описание: Секира простака.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 55";
+                        description.text = "Секира простака.";
+                        rank.text = "3";
+                        uron.text = "55";
                         real_uron = 55;
                         break;
                     case 26:
-                        description.text = "Описание: Секира пламенных чар.";
-                        rank.text = "Ранг: 5";
-                        uron.text = "Урон: 90";
+                        description.text = "Секира пламенных чар.";
+                        rank.text = "5";
+                        uron.text = "90";
                         real_uron = 90;
                         break;
                     case 27:
-                        description.text = "Описание: секира редкого металла.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 65";
+                        description.text = "секира редкого металла.";
+                        rank.text = "4";
+                        uron.text = "65";
                         real_uron = 65;
                         break;
                     case 28:
-                        description.text = "Описание: Посох заклинателя ядов.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 100";
+                        description.text = "Посох заклинателя ядов.";
+                        rank.text = "6";
+                        uron.text = "100";
                         real_uron = 100;
                         break;
                     case 29:
-                        description.text = "Описание: Потухшая волшебная палочка.";
-                        rank.text = "Ранг: 2";
-                        uron.text = "Урон: 30";
+                        description.text = "Потухшая волшебная палочка.";
+                        rank.text = "2";
+                        uron.text = "30";
                         real_uron = 30;
                         break;
                     case 30:
-                        description.text = "Описание: Палочка 'Глаз дракона'.";
-                        rank.text = "Ранг: 3";
-                        uron.text = "Урон: 45";
+                        description.text = "Палочка 'Глаз дракона'.";
+                        rank.text = "3";
+                        uron.text = "45";
                         real_uron = 45;
                         break;
                     case 31:
-                        description.text = "Описание: тюремный молот Печи.";
-                        rank.text = "Ранг: 4";
-                        uron.text = "Урон: 70";
+                        description.text = "тюремный молот Печи.";
+                        rank.text = "4";
+                        uron.text = "70";
                         real_uron = 70;
                         break;
                     case 32:
-                        description.text = "Описание: увесистый молот Печи.";
-                        rank.text = "Ранг: 5";
-                        uron.text = "Урон: 85";
+                        description.text = "увесистый молот Печи.";
+                        rank.text = "5";
+                        uron.text = "85";
                         real_uron = 85;
                         break;
                     case 33:
-                        description.text = "Описание: каменно-энергетический молот Печи.";
-                        rank.text = "Ранг: 6";
-                        uron.text = "Урон: 95";
+                        description.text = "каменно-энергетический молот Печи.";
+                        rank.text = "6";
+                        uron.text = "95";
                         real_uron = 95;
                         break;
                     case 34:
-                        description.text = "Описание: Кирка Начала Турнира.";
-                        rank.text = "Ранг: 1";
-                        uron.text = "Урон: 15";
+                        description.text = "Кирка Начала Турнира.";
+                        rank.text = "1";
+                        uron.text = "15";
                         real_uron = 15;
                         break;
                     case 35:
-                        description.text = "Описание: Вилы Начала Турнира.";
-                        rank.text = "Ранг: 1";
-                        uron.text = "Урон: 15";
+                        description.text = "Вилы Начала Турнира.";
+                        rank.text = "1";
+                        uron.text = "15";
                         real_uron = 15;
                         break;
                     case 36:
-                        description.text = "Описание: Лопата Начала Турнира.";
-                        rank.text = "Ранг: 1";
-                        uron.text = "Урон: 15";
+                        description.text = "Лопата Начала Турнира.";
+                        rank.text = "1";
+                        uron.text = "15";
                         real_uron = 15;
                         break;
                 }
@@ -283,9 +281,9 @@ public class Weap_Desc : MonoBehaviour
 
     public void OutHand()
     {
-        description.text = "Описание: Что ж, всего лишь ваши руки.";
-        rank.text = "Ранг: 0";
-        uron.text = "Урон: 0";
+        description.text = "Что ж, всего лишь ваши руки.";
+        rank.text = "0";
+        uron.text = "0";
         real_uron = 0;
 
         weap = false;
