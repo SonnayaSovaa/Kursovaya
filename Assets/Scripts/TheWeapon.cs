807e3e4f08ae17423eaee5192bfad330788bfa52
Ксения Нагишева <knagisheva@gmail.com>
2024-05-29 21:04:38 +0300

    донастройка оружия (не рябит)

--- Assets/Scripts/TheWeapon.cs
+++ Assets/Scripts/TheWeapon.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TheWeapon : MonoBehaviour
+using UnityEngine.XR.Interaction.Toolkit;
+public class TheWeapon : XRGrabInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject attach;
+
+    public virtual void AttachCustomReticle(XRBaseInteractor interactor) => AttachCustomReticle(interactor as IXRInteractor);
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        
+        SetParentToXRRig();
+        base.OnSelectEntered(args);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnSelectExited(SelectExitEventArgs args)
     {
-        
+        SetParentToWorld();
+        base.OnSelectExited(args);
+    }
+
+    public void SetParentToXRRig()
+    {
+        transform.SetParent(selectingInteractor.transform);
+    }
+
+    public void SetParentToWorld()
+    {
+        transform.SetParent(null);
     }
 }
+
