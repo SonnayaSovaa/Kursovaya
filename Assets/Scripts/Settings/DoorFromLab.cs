1a3c0e3bc79f44129bf6d4b84b80b189065ba920
Ксения Нагишева <knagisheva@gmail.com>
2024-06-02 23:45:42 +0300

    доработка

--- Assets/Scripts/Settings/DoorFromLab.cs
+++ Assets/Scripts/Settings/DoorFromLab.cs
@@ -20,20 +20,10 @@ public class DoorFromLab : MonoBehaviour
 
     Weap_Desc weapon;
 
-    string currScene;
-
-    Transform movingpoint;
-
-
 
     private void Start()
     {
-
-
-
         weapon = FindObjectOfType<Weap_Desc>();
-
-        currScene = SceneManager.GetActiveScene().name;
 
         controllers = FindObjectsOfType<ActionBasedController>();
         if (controllers[0].tag == "Right")
@@ -46,7 +36,14 @@ public class DoorFromLab : MonoBehaviour
             controller_L = controllers[0];
             controller_R = controllers[1];
         }
-    }
+
+        int mc = PlayerPrefs.GetInt("MagicCube");
+        int pp = PlayerPrefs.GetInt("PipeRotat");
+
+        if (this.name=="ToLes" && mc==1)
+            Destroy(this);
+        if (this.name=="ToStaem" && pp==1) Destroy(this);
+    } 
 
     private void Update()
     {
@@ -55,65 +52,16 @@ public class DoorFromLab : MonoBehaviour
 
     public void OnTriggerStay(Collider other)
     {
-        Debug.Log("Door1");
-        if (other.tag == "Left" && controller_L.selectAction.action.ReadValue<float>() >0 || other.tag == "Right" && controller_R.selectAction.action.ReadValue<float>() >0)
+        if (other.tag == "Left" && controller_L.selectAction.action.ReadValue<float>() > 0 || other.tag == "Right" && controller_R.selectAction.action.ReadValue<float>() > 0)
         {
-                       
-            Debug.Log("Door");
-
-
-            switch (currScene)
-            {
-
-                case "Steam_Lab":
-                    golov = PlayerPrefs.GetInt("PipeRotat");
-                    
-
-                    if (golov==1 && inHand)
-                        DoorOpen(this.name);
-                    break;
-
-
-                case "Forest":
-                    golov = PlayerPrefs.GetInt("MagicCube");
-                    
-                    if (golov == 1 && inHand)
-                        DoorOpen(this.name);
-                    break;
-
-
-                case "Start":
-                    
-                    if (this.name == "ToSteam")
-                    {
-                        golov = PlayerPrefs.GetInt("PipeRotat");
-                        if (golov == 0 && inHand)
-                            DoorOpen(this.name);
-                    }
-                    else if (this.name == "ToLes")
-                    {
-                        golov = PlayerPrefs.GetInt("MagicCube");
-                        if (golov == 0 && inHand)
-                            DoorOpen(this.name);
-                    }
-
-                    else if (this.name == "Arena")
-                    {
-                        golov = PlayerPrefs.GetInt("MagicCube") + PlayerPrefs.GetInt("PipeRotat");
-                        if (golov == 2 && inHand)
-                            DoorOpen(this.name);
-                    }
-
-                    break;
-            }
+            DoorOpen();
         }
 
     }
 
-    void DoorOpen(string name)
+    void DoorOpen()
     {
-        if (name=="Steam_Lab" || name=="Forest") door.transform.Translate(Vector3.left * 2 * Time.deltaTime);
-        else door.transform.Translate(Vector3.right * 2 * Time.deltaTime);
+        if (inHand) door.transform.Translate(Vector3.right * 2 * Time.deltaTime);
 
         
     }
